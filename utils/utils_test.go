fc33dac2a9aad575da2eaac12984383b0cb42902
QU35T-code <github@qu35t-mail.simplelogin.com>
2023-09-22 12:27:45 +0200

    Add TestGetMachineType

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"io"
+	"log"
 	"testing"
 )
 
@@ -8,5 +10,15 @@ func TestCheckHTBToken(t *testing.T) {
 	token := GetHTBToken()
 	if token == "" {
 		t.Fatal("Expected token but got empty variable")
+	}
+}
+
+func TestGetMachineType(t *testing.T) {
+	log.SetOutput(io.Discard)
+	var machine_id interface{}
+	machine_id = "558"
+	output := GetMachineType(machine_id, "")
+	if output != "retired" && output != "active" {
+		t.Fatalf("Expected \"%s\" but got \"%s\"", "active / retired", output)
 	}
 }
