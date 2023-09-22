2586d312d53679889eabafa2bee99fa376bd869f
QU35T-code <github@qu35t-mail.simplelogin.com>
2023-09-22 14:03:58 +0200

    Add utils tets

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -22,3 +22,35 @@ func TestGetMachineType(t *testing.T) {
 		t.Fatalf("Expected \"%s\" but got \"%s\"", "active / retired", output)
 	}
 }
+
+func TestGetUserSubscription(t *testing.T) {
+	log.SetOutput(io.Discard)
+	output := GetUserSubscription("")
+	if output != "vip+" && output != "vip" && output != "free" {
+		t.Fatalf("Expected \"%s\" but got \"%s\"", "free / vip / vip+", output)
+	}
+}
+
+func TestGetActiveMachineID(t *testing.T) {
+	log.SetOutput(io.Discard)
+	output := GetActiveMachineID("")
+	if output != "" {
+		t.Fatalf("Expected \"%s\" but got \"%s\"", "", output)
+	}
+}
+
+// func TestSearchItemIDByNameMachine(t *testing.T) {
+// 	log.SetOutput(io.Discard)
+// 	output := SearchItemIDByName("Sau", "", "Machine")
+// 	if output != "" {
+// 		t.Fatalf("Expected \"%s\" but got \"%s\"", "", output)
+// 	}
+// }
+
+// func TestSearchItemIDByNameChallenge(t *testing.T) {
+// 	log.SetOutput(io.Discard)
+// 	output := SearchItemIDByName("test", "", "Challenge")
+// 	if output != "" {
+// 		t.Fatalf("Expected \"%s\" but got \"%s\"", "", output)
+// 	}
+// }
