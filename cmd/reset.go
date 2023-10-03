0342c9fce505227aeee64d321eb951f2e1372772
QU35T-code <github@qu35t-mail.simplelogin.com>
2023-10-03 17:33:38 +0200

    Clean reset.go and add comments

--- cmd/reset.go
+++ cmd/reset.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,46 +10,67 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const baseAPIURL = "https://www.hackthebox.com/api/v4"
+
+// coreResetCmd sends a reset request for an active machine.
 func coreResetCmd(proxyParam string) (string, error) {
-	machine_id := utils.GetActiveMachineID(proxyParam)
-	if machine_id == "" {
+	// Retrieve the ID of the active machine.
+	machineID := utils.GetActiveMachineID(proxyParam)
+	if machineID == "" {
 		return "No active machine found", nil
 	}
-	log.Println("Machine ID :", machine_id)
-	machine_type := utils.GetMachineType(machine_id, "")
-	log.Println("Machine Type :", machine_type)
-
-	url := ""
-	jsonData := []byte("")
-	switch machine_type {
+	log.Printf("Machine ID: %s", machineID)
+	
+	// Retrieve the type of the machine.
+	machineType := utils.GetMachineType(machineID, "")
+	log.Printf("Machine Type: %s", machineType)
+	
+	// Determine the API endpoint and construct JSON data based on the machine type.
+	var endpoint string
+	switch machineType {
 	case "active":
-		url = "https://www.hackthebox.com/api/v4/vm/reset"
-		jsonData = []byte(`{"machine_id": ` + machine_id + `}`)
+		endpoint = "/vm/reset"
 	default:
-		url = "https://www.hackthebox.com/api/v4/arena/reset"
-		jsonData = []byte(`{"machine_id": ` + machine_id + `}`)
+		endpoint = "/arena/reset"
 	}
+	url := baseAPIURL + endpoint
+
+	// Construct JSON data.
+	jsonData, err := json.Marshal(map[string]string{"machine_id": machineID})
+	if err != nil {
+		return "", fmt.Errorf("failed to create JSON data: %w", err)
+	}
+
+	// Send an HTTP request to reset the machine.
 	resp, err := utils.HtbRequest(http.MethodPost, url, proxyParam, jsonData)
 	if err != nil {
 		return "", err
 	}
-	message := utils.ParseJsonMessage(resp, "message")
-	return message.(string), nil
+
+	// Parse and return the message from the response.
+	message, ok := utils.ParseJsonMessage(resp, "message").(string)
+	if !ok {
+		return "", fmt.Errorf("unexpected response format")
+	}
+	return message, nil
 }
 
+// resetCmd defines the "reset" command, which allows the user to reset a machine.
 var resetCmd = &cobra.Command{
 	Use:   "reset",
 	Short: "Reset a machine",
 	Long:  "Initiates a reset request for the selected machine.",
 	Run: func(cmd *cobra.Command, args []string) {
+		// Execute the core reset function and handle the results.
 		output, err := coreResetCmd(proxyParam)
 		if err != nil {
-			log.Fatal(err)
+			log.Fatalf("Error: %v", err)
 		}
 		fmt.Println(output)
 	},
 }
 
+// init adds the resetCmd to rootCmd, making it callable.
 func init() {
 	rootCmd.AddCommand(resetCmd)
 }
