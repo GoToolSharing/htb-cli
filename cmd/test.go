6c541d8a7ea0bcb496c176a643928784e71b2b55
QU35T-code <github@qu35t-mail.simplelogin.com>
2023-10-04 15:21:39 +0200

    Improve info GUI

--- cmd/test.go
+++ cmd/test.go
@@ -7,6 +7,8 @@ package cmd
 import (
 	"github.com/spf13/cobra"
 	"github.com/rivo/tview"
+	"github.com/gdamore/tcell/v2"
+	"strconv"
 )
 
 // testCmd represents the test command
@@ -61,12 +63,182 @@ to quickly create a Cobra application.`,
 		university.AddItem(tview.NewTextView().SetText("[::b]Rank[-]: [blue]40[-]").SetDynamicColors(true), 0, 1, false)
 		userCard.AddItem(university, 2, 0, false)
 
+		// TODO: Add challenges owned
+		// Add france rank
+		// Add fortress progression
+		// Add endgames progression
+		// Add last machine pwned
+		// Add prolabs progression
+
+		
+		// rankingCard.AddItem(tview.NewTextView().SetText("[::b]Player[-]: [blue]xct[-]").SetDynamicColors(true), 0, 1, false)
+		// rankingCard.AddItem(tview.NewTextView().SetText("[::b]Points[-]: [blue]3098[-]").SetDynamicColors(true), 0, 1, false)
+		// Création des entêtes
+		rankingTable := tview.NewTable().SetBorders(true)
+		rankingTable.SetCell(0, 0, tview.NewTableCell("Ranking").
+			SetTextColor(tcell.ColorWhite).
+			SetAttributes(tcell.AttrBold).
+			SetAlign(tview.AlignCenter))
+		rankingTable.SetCell(0, 1, tview.NewTableCell("Player").
+			SetTextColor(tcell.ColorWhite).
+			SetAttributes(tcell.AttrBold).
+			SetAlign(tview.AlignCenter))
+		rankingTable.SetCell(0, 2, tview.NewTableCell("Points").
+			SetTextColor(tcell.ColorWhite).
+			SetAttributes(tcell.AttrBold).
+			SetAlign(tview.AlignCenter))
+		rankingTable.SetCell(0, 3, tview.NewTableCell("Users").
+			SetTextColor(tcell.ColorWhite).
+			SetAttributes(tcell.AttrBold).
+			SetAlign(tview.AlignCenter))
+		rankingTable.SetCell(0, 4, tview.NewTableCell("Systems").
+			SetTextColor(tcell.ColorWhite).
+			SetAttributes(tcell.AttrBold).
+			SetAlign(tview.AlignCenter))
+		rankingTable.SetCell(0, 5, tview.NewTableCell("Challenges").
+			SetTextColor(tcell.ColorWhite).
+			SetAttributes(tcell.AttrBold).
+			SetAlign(tview.AlignCenter))
+
+		// Ajout des données de joueurs dans le tableau
+		playerData := []struct{
+			Name   string
+			Points string
+			Users string
+			Systems string
+			Challenges string
+		}{
+			{"Player1", "3000", "12", "13", "343"},
+			{"Player2", "2500", "19", "10", "363"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player1", "3000", "12", "13", "343"},
+			{"Player2", "2500", "19", "10", "363"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player1", "3000", "12", "13", "343"},
+			{"Player2", "2500", "19", "10", "363"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player1", "3000", "12", "13", "343"},
+			{"Player2", "2500", "19", "10", "363"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+			{"Player3", "2000", "22", "14", "353"},
+		}
+
+		for i, player := range playerData {
+			// Ajout du classement du joueur
+			rankingTable.SetCell(i+1, 0, tview.NewTableCell(strconv.Itoa(i+1)).
+				SetTextColor(tcell.ColorWhite).
+				SetAlign(tview.AlignCenter))
+			// Ajout du nom du joueur
+			rankingTable.SetCell(i+1, 1, tview.NewTableCell(player.Name).
+				SetTextColor(tcell.ColorWhite).
+				SetAlign(tview.AlignLeft))
+			// Ajout des points du joueur
+			rankingTable.SetCell(i+1, 2, tview.NewTableCell(string(player.Points)).
+				SetTextColor(tcell.ColorWhite).
+				SetAlign(tview.AlignRight))
+			// Ajout des users machines du joueur
+			rankingTable.SetCell(i+1, 3, tview.NewTableCell(string(player.Users)).
+				SetTextColor(tcell.ColorWhite).
+				SetAlign(tview.AlignRight))
+			// Ajout des systems machines du joueur
+			rankingTable.SetCell(i+1, 4, tview.NewTableCell(string(player.Systems)).
+				SetTextColor(tcell.ColorWhite).
+				SetAlign(tview.AlignRight))
+			// Ajout des challenges machines du joueur
+			rankingTable.SetCell(i+1, 5, tview.NewTableCell(string(player.Challenges)).
+				SetTextColor(tcell.ColorWhite).
+				SetAlign(tview.AlignRight))
+		}
+
+		rankingTable.SetSelectable(true, false) // Set to true, false to enable vertical scrolling
+		rankingTable.SetFixed(1, 0) // Sets the first row as non-scrollable
+		// Scrolling avec les touches fléchées
+		rankingTable.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+			switch event.Key() {
+			case tcell.KeyUp:
+				row, _ := rankingTable.GetSelection()
+				if row > 1 {
+					rankingTable.Select(row-1, 0)
+				}
+			case tcell.KeyDown:
+				row, _ := rankingTable.GetSelection()
+				if row < len(playerData) {  // assuming playerData is available in this context
+					rankingTable.Select(row+1, 0)
+				}
+			}
+			return event
+		})
+
+		// Ranking card
+		rankingCard := tview.NewFlex().SetDirection(tview.FlexRow)
+		rankingCard.SetBorder(true).SetTitle("Global Ranking").SetTitleAlign(tview.AlignLeft)
+		rankingCard.AddItem(rankingTable, 0, 1, false)
+
+
+	
+
+		// Flex droite basse
+		rightBottomFlex := tview.NewFlex().
+		AddItem(tview.NewBox().SetBorder(true).SetTitle("Country Ranking"), 0, 1, false)
+
+		// Flex droite qui contient les flex droite haute et droite basse
+		rightFlex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(rankingCard, 0, 1, false).
+		AddItem(rightBottomFlex, 0, 1, false)
+
+
 		// Main container
-		mainFlex := tview.NewFlex().SetDirection(tview.FlexColumn)
-		mainFlex.AddItem(userCard, 0, 1, true)
+		// mainFlex := tview.NewFlex().SetDirection(tview.FlexColumn)
+		// mainFlex.AddItem(userCard, 0, 2, true)
+		// mainFlex.AddItem(rankingCard, 0, 1, true)
+
+		mainFlex := tview.NewFlex().
+		AddItem(userCard, 0, 2, false).
+		AddItem(rightFlex, 0, 1, false)
+
 
 		// Run the application
-		if err := app.SetRoot(mainFlex, true).SetFocus(mainFlex).Run(); err != nil {
+		if err := app.SetRoot(mainFlex, true).SetFocus(rankingTable).Run(); err != nil {
 			panic(err)
 		}
 	},
