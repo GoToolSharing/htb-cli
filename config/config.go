ffeb20b2d8aba458e75cb3967867c9aa28dde3d0
Github Action <github-action@github.com>
2024-01-07 21:04:48 +0000

    Update dev version to 395883f75ab40db134f940c627d5b19b66c792c5

--- config/config.go
+++ config/config.go
@@ -32,7 +32,7 @@ const BaseHackTheBoxAPIURL = "https://" + HostHackTheBox + "/api/v4"
 
 const StatusURL = "https://status.hackthebox.com/api/v2/status.json"
 
-const Version = "v1.5.3"
+const Version = "395883f75ab40db134f940c627d5b19b66c792c5"
 
 func ConfigureLogger() error {
 	var logLevel zapcore.Level
